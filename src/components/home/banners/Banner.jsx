24433ef3365211d4e5d8c5f73941d0c6eba99865
drajendi <drajendi@cisco.com>
2023-07-18 22:07:40 +0530

    Fixing the transistion issues in image slider

--- src/components/home/banners/Banner.jsx
+++ src/components/home/banners/Banner.jsx
@@ -3,15 +3,17 @@ import './Banner.css'
 import watch from '../../../Images/img1.png'
 import speaker from '../../../Images/img2.png'
 import mobile from '../../../Images/img3.png'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 function Banner() {
   return (
     <div className='slider'>
         <div className='slider_images'>
-            <img src={watch}></img>
-            <img src={speaker}></img>
-            <img src={watch}></img>
-            <img src={speaker}></img>
-            <img src={mobile}></img>
+            <Link to='/watch'><img src={watch}></img></Link>
+            <Link to='/accessories'><img src={speaker}></img></Link>
+            <Link to='/phones'><img src={mobile}></img></Link>
+            <Link to='/watch'><img src={watch}></img></Link>
+            <Link to='/accessories'><img src={speaker}></img></Link>
+            <Link to='/phones'><img src={mobile}></img></Link>
         </div>
     </div>
   )
